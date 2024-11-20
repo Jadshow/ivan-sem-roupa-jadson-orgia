f0624fc87d1d356be375a966660d20e95c38d604
Jadshow <cjadson2006@gmail.com>
2024-11-19 22:24:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando foi o golpe militar do Brasil?',
+    '31 de Março de 1964'
+)
+
+criaCartao(
+    'história',
+    'Quando acabou a segunda guerra mundial?',
+    'em 1945'
+)
+
+criaCartao(
+    'História',
+    'quem foi o maior guerrilheiro do Brasil durante a ditadura?',
+    'Marighella'
+)
+
+criaCartao(
+    'Historia',
+    'quema foi o primeiro "presidente" da ditadura militar do brasil?',
+    'Marechal Castelo Branco'
+)
